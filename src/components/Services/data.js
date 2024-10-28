0836baa035077be6ba8017cb65a6f5f2eb04d162
Yahya <yahyahussein670@gmail.com>
2024-10-28 11:46:50 +0300

    add data retrival to the service section

---
+++ src/components/Services/data.js
@@ -0,0 +1,35 @@
+import beefIcon from '../../assets/images/icons/noun-beef-cuts-3029577-cropped.svg';
+import chickenIcon from '../../assets/images/icons/noun-chicken-cut-3029585-cropped.svg';
+import ramIcon from '../../assets/images/icons/noun-cuts-of-ram-3029581-cropped.svg';
+import goatIcon from '../../assets/images/icons/noun-goat-cuts-3029586-cropped.svg';
+import fishIcon from '../../assets/images/icons/noun-fish-7291296-cropped (1).svg';
+
+const data = [
+  { 
+    image: beefIcon,
+    title: 'Beef Supply',
+    description: "Chilled and frozen beef options available. Halal certified and delivered fresh to Gulf markets.",
+  },
+  { 
+    image: goatIcon,
+    title: 'Goat Supply',
+    description: "High-quality goat meat with stringent quality control checks. Available in custom cuts and packaging.",
+  },
+  { 
+    image: chickenIcon,
+    title: 'Chicken Supply',
+    description: "Chilled and frozen chicken, custom packed for client needs. Halal certified and delivered fresh.",
+  },
+  { 
+    image: ramIcon,
+    title: 'Lamb Supply',
+    description: "Top-tier lamb meat with strict quality control and freshness guaranteed. Available in chilled and frozen.",
+  },
+  { 
+    image: fishIcon,
+    title: 'Fish Supply',
+    description: "Sustainably sourced and freshly harvested, our fish exports are a staple in the Gulf region. We supply a range of fish, ensuring top-quality packaging for longer shelf life and safe transport.",
+  }
+];
+
+export default data;
